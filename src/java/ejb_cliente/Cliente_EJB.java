a5719d535d8706df53bc26f6cc019fbfad0d7292
Leonardo Flores <leonardo_783.floher@hotmail.com>
2024-05-05 18:33:43 -0600

    Rama creada

---
+++ src/java/ejb_cliente/Cliente_EJB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejb_cliente;
+
+/**
+ *
+ * @author leona
+ */
+public class Cliente_EJB {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Soy el cliente");
+    }
+}
